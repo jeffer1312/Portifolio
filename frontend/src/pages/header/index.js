c0e97b10787b152bc8770133ba724219c4518926
Jefferson Felizardo <jeffer1312@gmail.com>
2020-04-22 15:09:20 -0300

    Adicionando React Typical

--- frontend/src/pages/header/index.js
+++ frontend/src/pages/header/index.js
@@ -1,42 +1,84 @@
-import React from "react";
-import logoImg from "../../assets/Logo.png";
-import headerImg from "../../assets/header.jpg";
+import React, { useState, useEffect } from "react";
+import Typical from "react-typical";
+
+import logoImg from "../../assets/Logo1.png";
+
 import gitHubImg from "../../assets/github.svg";
 import {
   Container,
-  H1,
+  TitleText,
   Img,
   Logo,
   Nav,
   Ul,
   Li,
   A,
-  HeaderImg,
-  HEADER
+  HEADER,
+  Description,
+  DescriptionText
 } from "./styles";
 
 export default function Header() {
+  const [renderMsg, setRenderMsg] = useState(false);
+  const teste = ["Desenvolvedor FullStack ", "Programador JavaScript "];
+  const [frase, setFrase] = useState(teste[0]);
+
   return (
-    <HEADER>
-      <HeaderImg src={headerImg} alt="header" />
-      <Container>
-        <Logo>
-          <Img src={logoImg} alt="Jefferson Felizardo" />
-        </Logo>
-        <Nav>
-          <Ul>
-            <Li>
-              <A href="/">Home</A>
-            </Li>
-            <Li>
-              <A href="/">Sobre</A>
-            </Li>
-            <Li>
-              <A href="/">Contato</A>{" "}
-            </Li>
-          </Ul>
-        </Nav>
-      </Container>
-    </HEADER>
+    <>
+      <HEADER>
+        <Container>
+          <Logo>
+            <Img src={logoImg} alt="Jefferson Felizardo" />
+          </Logo>
+          <Nav>
+            <Ul>
+              <Li>
+                <A href="/">Home</A>
+              </Li>
+              <Li>
+                <A href="/">Sobre</A>
+              </Li>
+              <Li>
+                <A href="/">Contato</A>
+              </Li>
+            </Ul>
+          </Nav>
+        </Container>
+        <Description>
+          <Container column>
+            <TitleText>Jefferson Felizardo</TitleText>
+            <DescriptionText>
+              <Typical
+                steps={[
+                  "Desenvolvedor FullStack ",
+                  1500,
+                  "Programador JavaScript ",
+                  1500
+                ]}
+                loop={Infinity}
+              />
+            </DescriptionText>
+            <DescriptionText secondary>
+              <Typical
+                steps={[
+                  "React-Js ",
+                  4000,
+                  "Node-Js ",
+                  4000,
+                  "Sql-Lite ",
+                  2500,
+                  "MySql ",
+
+                  2500,
+                  "MongoDb ",
+                  2500
+                ]}
+                loop={Infinity}
+              />
+            </DescriptionText>
+          </Container>
+        </Description>
+      </HEADER>
+    </>
   );
 }
--- frontend/src/pages/header/styles.js
+++ frontend/src/pages/header/styles.js
@@ -1,29 +1,34 @@
 import styled from "styled-components";
+import headerImg from "../../assets/header1.jpg";
 
 export const Container = styled.div`
-  position: relative;
-  top: -1280px;
   padding: 0 2%;
   margin: 0 auto;
   display: flex;
+  flex-direction: ${props => (props.column ? "column" : "row")};
   max-width: 1400px;
 `;
+
 export const HEADER = styled.header`
+  background-image: url(${headerImg});
   width: 100%;
+  background-attachment: fixed;
+  max-width: 1920px;
+  height: 700px;
   position: relative;
 `;
-export const HeaderImg = styled.img`
-  opacity: 0.6;
-`;
+
 export const Nav = styled.nav`
   color: white;
+  padding-top: 40px;
 `;
-
+export const Description = styled.section``;
 export const Ul = styled.ul`
   display: flex;
   padding: 40px;
   list-style-type: none;
 `;
+
 export const Li = styled.li`
   padding: 0 30px;
 `;
@@ -32,25 +37,31 @@ export const A = styled.a`
   color: white;
   font-size: 25px;
 `;
-export const H1 = styled.h1`
-  color: red;
+export const TitleText = styled.h1`
+  font-weight: bold;
+  font-size: 80px;
+  line-height: 100px;
+  -webkit-text-stroke-width: 2.3px;
+  -webkit-text-stroke-color: #666;
+  color: #fff;
 `;
+export const DescriptionText = styled.h1`
+  padding-top: 4%;
+  color: ${props => (props.secondary ? "white" : "#0779e4")};
+
+  font-style: normal;
+  font-weight: bold;
+  font-size: ${props => (props.secondary ? "36px" : "30px")};
+  line-height: 35px;
+`;
+
 export const Logo = styled.div`
-  margin: 0;
-  padding: 0;
-  width: 60%;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: flex-start;
+  min-width: 60%;
 `;
 
 export const Img = styled.img`
   width: 100%;
   height: 100%;
-  max-width: 900px;
-  max-height: 1080px;
-  position: relative;
-  left: -35%;
-  top: -112px;
+  max-width: 400px;
+  max-height: 180px;
 `;
